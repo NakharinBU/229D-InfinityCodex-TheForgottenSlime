cb662dc6be4b65d8c6e97041d5ce3ed3695b25f0
NakharinBU <nakharin.sise@bumail.net>
2025-03-28 14:16:45 +0700

    Update fix ui bug

--- Assets/Scripts/EndCreditScroll.cs
+++ Assets/Scripts/EndCreditScroll.cs
@@ -7,27 +7,11 @@ using UnityEngine.SceneManagement;
 public class EndCreditScroll : MonoBehaviour
 {
     public RectTransform endGameText;
-    public RectTransform creditText;
     public float scrollSpeed = 50f; 
 
     void Start()
     {
-        if (endGameText != null)
-        {
-            endGameText.DOAnchorPosY(2100, scrollSpeed).SetEase(Ease.Linear).OnComplete(() =>
-            {
-                Debug.Log("End Game");
-            });
-        }
-        else if (endGameText == null) return;
-
-        if (creditText != null)
-        {
-            creditText.DOAnchorPosY(2100, scrollSpeed).SetEase(Ease.Linear).OnComplete(() =>
-            {
-                SceneManager.LoadScene(0);
-            });
-        }
-        else if (creditText == null) return;
+        endGameText.DOAnchorPosY(2100, scrollSpeed).SetEase(Ease.Linear).OnComplete(() =>
+            {Debug.Log("End Game");});
     }
 }
